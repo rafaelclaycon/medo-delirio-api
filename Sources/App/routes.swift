9a97cf432ea660bd640bf681ca13a586b7f04ee5
Rafael Schmitt <rafael.claycon@gmail.com>
2022-06-03 12:22:48 -0300

    Add /api/SoundShareCountStats route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,9 +1,10 @@
 import Fluent
 import Vapor
+import SQLiteNIO
 
 func routes(_ app: Application) throws {
     app.get { req in
-        return "Shantay you stay! medo-delirio-api is up and running."
+        return "Servidor OK."
     }
 
     app.get("hello", ":name") { req -> String in
@@ -20,8 +21,26 @@ func routes(_ app: Application) throws {
         }
     }
     
-    app.get("api", "ShareCountStats") { req -> EventLoopFuture<[ShareCountStat]> in
+    app.get("api", "AllSoundShareCountStats") { req -> EventLoopFuture<[ShareCountStat]> in
         ShareCountStat.query(on: req.db).all()
+    }
+    
+    app.get("api", "SoundShareCountStats") { req -> EventLoopFuture<[ShareCountStat]> in
+        if let sqlite = req.db as? SQLiteDatabase {
+            let query = """
+                select s.contentId, sum(s.shareCount) totalShareCount
+                from ShareCountStat s
+                where s.contentType = 0
+                group by s.contentId
+                order by totalShareCount desc
+            """
+            
+            return sqlite.query(query).flatMapEach(on: req.eventLoop) { row in
+                req.eventLoop.makeSucceededFuture(ShareCountStat(installId: "", contentId: row.column("contentId")?.string ?? "", contentType: 0, shareCount: row.column("totalShareCount")?.integer ?? 0))
+            }
+        } else {
+            return req.eventLoop.makeSucceededFuture([ShareCountStat]())
+        }
     }
 
     //try app.register(collection: TodoController())
