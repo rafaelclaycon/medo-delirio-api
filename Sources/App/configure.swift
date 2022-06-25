616154ef9ad0a3c626fa56f430a3d8efd3e27bad
Rafael Schmitt <rafael.claycon@gmail.com>
2022-06-24 23:26:16 -0300

    Add CORS policy

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,6 +6,14 @@ import Vapor
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    app.middleware.use(cors, at: .beginning)
 
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
 
