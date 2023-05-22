3d15dbb8ac259bd85d811af0c1d66d76b98e901a
Rafael Schmitt <rafael.claycon@gmail.com>
2023-05-22 12:34:04 -0300

    Move remaining endpoints

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,7 +1,11 @@
-import Fluent
+//
+//  routes.swift
+//  medo-delirio-api
+//
+//  Created by Rafael Schmitt on 01/06/22.
+//
+
 import Vapor
-import SQLiteNIO
-import APNS
 
 func routes(_ app: Application) throws {
     let statusCheckController = StatusCheckController()
@@ -30,62 +34,21 @@ func routes(_ app: Application) throws {
     app.post("api", "v3", "set-donor-names", ":password", use: donorsController.postSetDonorNamesHandlerV3)
     
     let clientLoggingController = ClientLoggingController()
-    app.post("api", "v1", "client-device-info") { req -> EventLoopFuture<ClientDeviceInfo> in
-        let info = try req.content.decode(ClientDeviceInfo.self)
-        return info.save(on: req.db).map {
-            info
-        }
-    }
-    app.post("api", "v1", "user-folder-logs") { req -> HTTPStatus in
-        let folderLogs = try req.content.decode([UserFolderLog].self)
-        
-        try await req.db.transaction { transaction in
-            for log in folderLogs {
-                try await log.save(on: transaction)
-            }
-        }
-        return .ok
-    }
-    app.post("api", "v1", "user-folder-content-logs") { req -> HTTPStatus in
-        let contentLogs = try req.content.decode([UserFolderContentLog].self)
-        
-        try await req.db.transaction { transaction in
-            for log in contentLogs {
-                try await log.save(on: transaction)
-            }
-        }
-        return .ok
-    }
-    app.post("api", "v1", "still-alive-signal") { req -> HTTPStatus in
-        let signal = try req.content.decode(StillAliveSignal.self)
-        
-        try await req.db.transaction { transaction in
-            try await signal.save(on: transaction)
-        }
-        return .ok
-    }
-    app.post("api", "v2", "usage-metric") { req -> HTTPStatus in
-        let metric = try req.content.decode(UsageMetric.self)
-        try await req.db.transaction { transaction in
-            try await metric.save(on: transaction)
-        }
-        return .ok
-    }
+    app.post("api", "v1", "client-device-info", use: clientLoggingController.postClientDeviceInfoHandlerV1)
+    app.post("api", "v1", "user-folder-logs", use: clientLoggingController.postUserFolderLogsHandlerV1)
+    app.post("api", "v1", "user-folder-content-logs", use: clientLoggingController.postUserFolderContentLogsHandlerV1)
+    app.post("api", "v1", "still-alive-signal", use: clientLoggingController.postStillAliveSignalHandlerV1)
+    app.post("api", "v2", "usage-metric", use: clientLoggingController.postUsageMetricHandlerV2)
     
     let notificationsController = NotificationsController()
-    app.post("api", "v1", "push-device") { req -> EventLoopFuture<PushDevice> in
-        let device = try req.content.decode(PushDevice.self)
-        return device.save(on: req.db).map {
-            device
-        }
-    }
+    app.post("api", "v1", "push-device", use: notificationsController.postPushDeviceHandlerV1)
     app.post("api", "v1", "send-push-notification") { req -> HTTPStatus in
         let notif = try req.content.decode(PushNotification.self)
         
         guard let password = notif.password, password == ReleaseConfigs.Passwords.sendNotificationPassword else {
             throw Abort(.unauthorized)
         }
-
+        
         let devices = try await PushDevice.query(on: req.db).all()
         for device in devices {
             _ = app.apns.send(.init(title: notif.title, body: notif.description), to: device.pushToken)
@@ -93,11 +56,5 @@ func routes(_ app: Application) throws {
         }
         return .ok
     }
-//    app.post("api", "v2", "add-all-existing-devices-to-general-channel") { req -> HTTPStatus in
-//        PushDevice.query(on: req.db).all().flatMapEach(on: req.eventLoop) { device in
-//            let deviceChannel = try DeviceChannel(id: UUID(), device: PushDevice(installId: device.installId, pushToken: device.pushToken), channel: PushChannel(id: UUID(), channelId: "general"))
-//            try deviceChannel.save(on: req.db)
-//        }
-//        return HTTPStatus.ok
-//    }
+    //app.post("api", "v2", "add-all-existing-devices-to-general-channel", use: notificationsController.postAddAllExistingDevicesToGeneralChannelHandlerV2)
 }
