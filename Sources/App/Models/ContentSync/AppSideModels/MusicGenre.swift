4e55a553eb9d44f844e4d3bdc9b3e2594e7af916
Rafael Schmitt <rafael.claycon@gmail.com>
2023-06-19 16:02:21 -0300

    Update MusicGenre enum

--- Sources/App/Models/ContentSync/AppSideModels/MusicGenre.swift
+++ Sources/App/Models/ContentSync/AppSideModels/MusicGenre.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum MusicGenre: String, CaseIterable, Identifiable, Codable {
 
-    case all, arrocha, electronic, funk, undefined, house, jingle, marchinha, metal, mpb, pagode, pisero, pop, rock, samba, sertanejo, tecno, variousGenres
+    case all, arrocha, electronic, funk, undefined, house, jingle, marchinha, metal, mpb, pagode, pisero, pop, reggae, rock, samba, sertanejo, tecno, variousGenres
     
     var id: String { String(self.rawValue) }
 }
