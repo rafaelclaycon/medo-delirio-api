59dfc5db9f739553dd5223fc298bc17253a42f39
Rafael Schmitt <rafaelschmitt@fastmail.com>
2026-02-25 19:34:07 -0300

    Fix migration

--- Sources/App/Migrations/AddNotificationTrackingToLastKnownEpisode.swift
+++ Sources/App/Migrations/AddNotificationTrackingToLastKnownEpisode.swift
@@ -5,7 +5,11 @@ struct AddNotificationTrackingToLastKnownEpisode: AsyncMigration {
     func prepare(on database: Database) async throws {
         try await database.schema("LastKnownEpisode")
             .field("lastNotificationSentCount", .int)
+            .update()
+        try await database.schema("LastKnownEpisode")
             .field("lastNotifiedDeviceIds", .string)
+            .update()
+        try await database.schema("LastKnownEpisode")
             .field("lastNotifiedAt", .datetime)
             .update()
     }
@@ -13,7 +17,11 @@ struct AddNotificationTrackingToLastKnownEpisode: AsyncMigration {
     func revert(on database: Database) async throws {
         try await database.schema("LastKnownEpisode")
             .deleteField("lastNotificationSentCount")
+            .update()
+        try await database.schema("LastKnownEpisode")
             .deleteField("lastNotifiedDeviceIds")
+            .update()
+        try await database.schema("LastKnownEpisode")
             .deleteField("lastNotifiedAt")
             .update()
     }
